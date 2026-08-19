e597f5faa8cc94e08da79730f1f936adcf95c9a4
Yakup Aluc <yalucc0@gmail.com>
2026-08-19 15:48:21 +0200

    feat: add custom teal theme with proper light and dark schemes

--- app/src/main/java/com/yakupaluc/habitflow/ui/theme/Theme.kt
+++ app/src/main/java/com/yakupaluc/habitflow/ui/theme/Theme.kt
@@ -12,32 +12,48 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = md_primary_dark,
+    onPrimary = md_onPrimary_dark,
+    primaryContainer = md_primaryContainer_dark,
+    onPrimaryContainer = md_onPrimaryContainer_dark,
+    secondary = md_secondary_dark,
+    onSecondary = md_onSecondary_dark,
+    secondaryContainer = md_secondaryContainer_dark,
+    onSecondaryContainer = md_onSecondaryContainer_dark,
+    tertiary = md_tertiary_dark,
+    onTertiary = md_onTertiary_dark,
+    background = md_background_dark,
+    onBackground = md_onBackground_dark,
+    surface = md_surface_dark,
+    onSurface = md_onSurface_dark,
+    surfaceVariant = md_surfaceVariant_dark,
+    onSurfaceVariant = md_onSurfaceVariant_dark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = md_primary_light,
+    onPrimary = md_onPrimary_light,
+    primaryContainer = md_primaryContainer_light,
+    onPrimaryContainer = md_onPrimaryContainer_light,
+    secondary = md_secondary_light,
+    onSecondary = md_onSecondary_light,
+    secondaryContainer = md_secondaryContainer_light,
+    onSecondaryContainer = md_onSecondaryContainer_light,
+    tertiary = md_tertiary_light,
+    onTertiary = md_onTertiary_light,
+    background = md_background_light,
+    onBackground = md_onBackground_light,
+    surface = md_surface_light,
+    onSurface = md_onSurface_light,
+    surfaceVariant = md_surfaceVariant_light,
+    onSurfaceVariant = md_onSurfaceVariant_light,
 )
 
 @Composable
 fun HabitFlowTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
