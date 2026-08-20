da3527a92a8349bfd6d8859b3a939fb562a8dfcc
Yakup Aluc <yalucc0@gmail.com>
2026-08-20 15:20:02 +0200

    feat: add weekly completion stats calculation

---
+++ app/src/main/java/com/yakupaluc/habitflow/ui/habitdetail/WeeklyStats.kt
@@ -0,0 +1,26 @@
+package com.yakupaluc.habitflow.ui.habitdetail
+
+data class WeekBar(
+    val weekIndex: Int,
+    val completedCounts: Int
+)
+
+fun buildWeeklyStats(
+    completedDates: Set<Long>,
+    todayEpochDay: Long,
+    weekCount: Int = 7
+): List<WeekBar> {
+    val daysPerWeek = 7
+    val totalDays = weekCount * daysPerWeek
+    val startDay = todayEpochDay - (totalDays - 1)
+
+    return (0 until weekCount).map { week ->
+        val weekStart = startDay + week * daysPerWeek
+        val weekEnd = weekStart + daysPerWeek - 1
+        val count = completedDates.count() { it in weekStart..weekEnd }
+        WeekBar(
+            weekIndex = week,
+            completedCounts = count
+        )
+    }
+}
